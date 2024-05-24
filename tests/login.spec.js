8503204dfd8ccc8825c74d06139007d3916eeede
Mariia Bagatska <mariia.bagatska@gmail.com>
2024-05-24 10:25:40 +0200

    update test data and adjust login tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,15 +1,16 @@
-const {test, expect} = require(`@playwright/test`);
-const validUsers = require (`../test-data/valid-users.json`);
-const invalidUsers = require(`../test-data/invalid-users.json`);
-const LoginPage = require(`../pom/LoginPage`);
+const { test, expect } = require('@playwright/test');
+const testUsers = require('../test-data/test-users.json');
+const LoginPage = require('../pom/LoginPage');
+const validUsers = testUsers.validUsers;
+const invalidUsers = testUsers.invalidUsers;
 
-test.beforeEach(async ({page}) => {
+test.beforeEach(async ({ page }) => {
     console.log(`Starting ${test.info().title}`);
-    console.log (`Opening login page`);
-    await page.goto('./'); 
+    console.log('Opening login page');
+    await page.goto('./');
 });
 
-test.afterEach(async ({page}) => {
+test.afterEach(async ({ page }) => {
     console.log(`Finished ${test.info().title} with status ${test.info().status}`);
 
     if (test.info().status != test.info().expectedStatus) {
@@ -17,59 +18,59 @@ test.afterEach(async ({page}) => {
     }
 });
 
-test.describe(`Functional testing`, () => {
+test.describe('Functional testing', () => {
 
     for (let i = 0; i < validUsers.length; i++) {
 
-        test(`Successful login for ${validUsers[i].username}`, async ({page}) => {
+        test(`Successful login for ${validUsers[i].username}`, async ({ page }) => {
             const loginPage = new LoginPage(page);
             await loginPage.userLogsIn(validUsers[i].username, validUsers[i].password);
             await expect(page).toHaveURL(validUsers[i].expectedUrl);
 
         });
     }
-    
-    for(let i = 0; i < invalidUsers.length; i++) {
 
-        test(`Login ${invalidUsers[i].scenario}`, async ({page}) => {
+    for (let i = 0; i < invalidUsers.length; i++) {
+
+        test(`Login ${invalidUsers[i].scenario}`, async ({ page }) => {
             const loginPage = new LoginPage(page);
             await loginPage.userLogsIn(invalidUsers[i].username, invalidUsers[i].password);
-            let expcetedErrorText = invalidUsers[i].error;
+            let expectedErrorText = invalidUsers[i].error;
             let actualErrorText = await loginPage.getErrorMessage();
-            expect(actualErrorText).toBe(expcetedErrorText);
+            expect(actualErrorText).toBe(expectedErrorText);
             await expect(page).toHaveURL(invalidUsers[i].expectedUrl);
         });
-       
+
     }
 });
 
-    test.describe(`Security testing`, ()=> {
-        
-        test.fail(`Brute-force attack prevention`, async ({page}) => {
-            for (let i = 0; i < 5; i++ ){
-                console.log(`Login attempt # ${i}`);
-                let validUsername = 'standard-user';
-                let invalidPassword = 'secret-saucee'
-                const loginPage = new LoginPage(page);
-                await loginPage.userLogsIn(validUsername, invalidPassword);
-            }    
-            await expect(page).toHaveURL(/locked/);
-        });
+test.describe('Security testing', () => {
 
-        test(`Session management after inactivity`, async ({page}) => {
-            test.setTimeout(720000);
+    test.fail('Brute-force attack prevention', async ({ page }) => {
+        for (let i = 0; i < 5; i++) {
+            console.log(`Login attempt # ${i}`);
+            let validUsername = 'standard-user';
+            let invalidPassword = 'secret-saucee'
             const loginPage = new LoginPage(page);
-            await loginPage.userLogsIn(validUsers[0].username, validUsers[0].password);
-            await expect(page).toHaveURL(validUsers[0].expectedUrl);
-            console.log(`Simulating inactivity for 11 minutes`);
-            await page.waitForTimeout(660000);
-            await page.goto(validUsers[0].expectedUrl);
-            await expect(page).toHaveURL('./');
+            await loginPage.userLogsIn(validUsername, invalidPassword);
+        }
+        await expect(page).toHaveURL(/locked/);
+    });
 
-        });
-
-        
+    test('Session management after inactivity', async ({ page }) => {
+        test.setTimeout(720000);
+        const loginPage = new LoginPage(page);
+        await loginPage.userLogsIn(validUsers[0].username, validUsers[0].password);
+        await expect(page).toHaveURL(validUsers[0].expectedUrl);
+        console.log('Simulating inactivity for 11 minutes');
+        await page.waitForTimeout(660000);
+        await page.goto(validUsers[0].expectedUrl);
+        await expect(page).toHaveURL('./');
 
     });
-    
+
+
+
+});
+
 
